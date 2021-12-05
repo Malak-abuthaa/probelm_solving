64010478c0200443cd007041426ae2ebe3a5f1dc
malak.abuthraa@gmail.com <195mm41>
2021-12-05 09:23:46 +0200

    update

---
+++ Manara week #4/Task 1 implement hash.py
@@ -0,0 +1,9 @@
+""""
+mplement a class called HashTable in your favorite programming language.
+
+Assume that keys will always be integers for simplicity (i.e. you can use the modulo hash function we used in the lecture slides)
+Should take two constructor parameters: initialCapacity (m), and loadFactor (default is 0.75)
+Should implement and expose the following methods: put(k, v), get(k), and delete(k)
+[execution time limit] 4 seconds (py3)
+"""
+
