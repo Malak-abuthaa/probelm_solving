cf34a80a763c2cb06f1c0c5738b3c5c83347f6dd
Malak-abuthaa <malak.abuthraa@gmail.com>
2021-12-11 23:21:27 +0200

    update

---
+++ Manara Week #5/Is symmetric.py
@@ -0,0 +1,102 @@
+""""Given a binary tree t, determine whether it is symmetric around its center, i.e. each side mirrors the other.
+
+Example
+
+For
+
+t = {
+    "value": 1,
+    "left": {
+        "value": 2,
+        "left": {
+            "value": 3,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 4,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 2,
+        "left": {
+            "value": 4,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be isTreeSymmetric(t) = true.
+
+Here's what the tree in this example looks like:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+As you can see, it is symmetric.
+
+For
+
+t = {
+    "value": 1,
+    "left": {
+        "value": 2,
+        "left": null,
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 2,
+        "left": null,
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be isTreeSymmetric(t) = false.
+
+Here's what the tree in this example looks like:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+As you can see, it is not symmetric."""
+
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def solution(t):
+    if t is None:
+        return None
+
+    def symmeric_helper(node_left, node_right):
+        if node_left is None and node_right is None:
+            return True
+        elif node_left is None or node_right is None:
+            return False
+        return node_left.value == node_right.value and symmeric_helper(node_left.left,
+                                                                       node_right.right) and symmeric_helper(
+            node_left.right, node_right.left)
+
+    return symmeric_helper(t.left, t.right)
+
